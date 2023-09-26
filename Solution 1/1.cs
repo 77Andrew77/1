a56845631f06e196a7f3e487820e92a581d90eec
77Andrew77 <146068547+77Andrew77@users.noreply.github.com>
2023-09-26 12:45:46 +0300

    Add files via upload

---
+++ Solution 1/1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Solution_1
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Введите любое число: ");
+            string n = Console.ReadLine();
+            string[] numbers = n.Split(' ');
+            
+            foreach (string numberString in numbers)
+            {
+                //попытка преобразования строки в число
+                if (double.TryParse(numberString, out double number))
+                {
+                    //проверка на деление без остатка
+                    if ((number % 2) == 0)
+                    {
+                        Console.WriteLine($"{number} - Четное");
+                    }
+                    else if ((number % 2) != 0)
+                    {
+                        Console.WriteLine($"{number} - Не четное");
+                    }
+                }
+                //если не число, то выдаст ошибку
+                else { Console.WriteLine($"Не верный ввод ({numberString}). Число либо не целое, либо введен"); }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Solution 4/4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            //объявление массива и переменных
+            int[] znach = new int[1000];
+            int min = znach[0]; int minIndex = 0;
+            int max = znach[0]; int maxIndex = 0;
+            Console.WriteLine("Введите размер массива: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            Console.WriteLine("Массив");
+            //создание массива
+            for (int i = 0; i < x; i++)
+            {
+                znach[i] = random.Next(-200, 200);
+                Console.WriteLine($"{i}: " + znach[i]);
+                //присвоение минимального значения и индекса
+                if (min > znach[i])
+                {
+                    min = znach[i];
+                    minIndex = i;
+                }
+                //присвоение максимального значения и индекса
+                else if (max < znach[i])
+                {
+                    max = znach[i];
+                    maxIndex = i;
+                }
+            }
+            Console.WriteLine("Максимальное число: " + maxIndex + " = " + max);
+            Console.WriteLine("Минимальное число: " + minIndex + " = " + min);
+        }
+    }
+}
